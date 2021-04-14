00111a8a8c03a0447fd7cd7d17907c2f11a1d214
lewiswolf <lewiswolstanholme@gmail.com>
2021-04-14 19:11:36 +0100

    added Bela API and now connects/disconnects successfully on Chromium

--- react-app/src/components/App.jsx
+++ react-app/src/components/App.jsx
@@ -1,18 +1,36 @@
+// packages
 import React from 'react'
-import P5 from './p5'
+import Bela from '../Bela API'
 
+// components
 import Head from './head'
 
+// scss
 import '../scss/App.scss'
 
-export default function App() {
-	return (
-		<React.Fragment>
-			<Head description='2-dimensional physically modelled drum running on Bela' />
-			<p>some text for now</p>
-			<div style={{ height: 300, width: 300, placeSelf: 'center' }} className='p5-container'>
-				<P5 src='../sketch/drum.js' />
-			</div>
-		</React.Fragment>
-	)
+export default class App extends React.Component {
+	constructor(props) {
+		super(props)
+		this.state = {
+			belaLoaded: Bela.readyState === 1 ? true : false, // is the Bela ws connected?
+		}
+	}
+
+	componentDidMount() {
+		// listeners for when Bela is connected/disconnected
+		window.addEventListener('BelaConnected', () => this.setState({ belaLoaded: true }))
+		window.addEventListener('BelaDisconnected', () => this.setState({ belaLoaded: false }))
+	}
+
+	render() {
+		return (
+			<React.Fragment>
+				<Head description='2-dimensional physically modelled drum running on Bela' />
+				<p style={{ display: !this.state.belaLoaded ? 'block' : 'none' }}>
+					There is no Bela connected... 🧑‍💻
+				</p>
+				<main style={{ display: this.state.belaLoaded ? 'block' : 'none' }}></main>
+			</React.Fragment>
+		)
+	}
 }
---
+++ readme.md
@@ -0,0 +1,11 @@
+To use this app, install this repo on both a Bela and your personal machine, both of which need to be connected to the same network. Then start the project on http://bela.local and switch over to your local machine to install the GUI.
+
+As a pre-requisite, you will need NodeJS installed, which is available from most established package manages (apt, homebrew, etc...) or you can download it directly online. Then, from inside the project directory run
+
+```bash
+cd react-app
+npm i
+npm start
+```
+
+This will launch the GUI in your browser, at http://localhost:3000, and upon successfully connecting to Bela you can begin playing. :)
