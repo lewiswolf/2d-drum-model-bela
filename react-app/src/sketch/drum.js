9960f82ed7ef6678758e76cb992a8de17be36d0c
lewiswolf <lewiswolstanholme@gmail.com>
2021-05-04 12:36:14 +0100

    converted to vanilla dom events as these seemed to be faster than p5

--- react-app/src/sketch/drum.js
+++ react-app/src/sketch/drum.js
@@ -1,30 +1,35 @@
 export default function Drum(props, p5) {
 	class Drum {
+		// private
 		#mouseState
+		// methods
+		#isInsideDrum
+		#polarCoords
 
 		constructor(props) {
 			// public
 			this.x = props?.x || 0
 			this.y = props?.y || 0
 			this.diameter = props?.diameter || 0
+
 			// methods
 			this.mouseDown = props?.mouseDown || null
 			this.mouseUp = props?.mouseUp || null
 			this.mouseDrag = props?.drag || null
 			this.mouseDragExit = props?.dragExit || null
+
 			// private
 			this.#mouseState = false
-		}
 
-		handleEvents = () => {
-			let isInsideDrum = p5.dist(
-				p5.mouseX,
-				p5.mouseY,
+			// methods
+			this.#isInsideDrum = (mousex, mousey) => p5.dist(
+				mousex,
+				mousey,
 				this.x,
 				this.y
 			) < this.diameter / 2
 
-			const polarCoords = (mousex, mousey) => {
+			this.#polarCoords = (mousex, mousey) => {
 				const x = mousex - this.x
 				const y = mousey - this.y
 				return {
@@ -33,25 +38,29 @@ export default function Drum(props, p5) {
 				}
 			}
 
-			// mousedown
-			if (p5.mouseIsPressed && !this.#mouseState && isInsideDrum) {
-				this.mouseDown && this.mouseDown(polarCoords(p5.mouseX, p5.mouseY))
-				this.#mouseState = true
-			}
-			// mouseup
-			if (!p5.mouseIsPressed && this.#mouseState && isInsideDrum) {
-				this.mouseUp && this.mouseUp(polarCoords(p5.mouseX, p5.mouseY))
-				this.#mouseState = false
-			}
-			// drag
-			if (p5.mouseIsPressed && this.#mouseState && isInsideDrum) {
-				this.mouseDrag && this.mouseDrag(polarCoords(p5.mouseX, p5.mouseY))
-			}
-			// dragexit
-			if (p5.mouseIsPressed && this.#mouseState && !isInsideDrum) {
-				this.mouseDragExit && this.mouseDragExit(polarCoords(p5.mouseX, p5.mouseY))
-				this.#mouseState = false
-			}
+			// listeners
+			window.addEventListener('mousedown', (e) => {
+				if (this.#isInsideDrum(p5.mouseX, p5.mouseY) && e.button === 0) {
+					this.mouseDown && this.mouseDown(this.#polarCoords(p5.mouseX, p5.mouseY))
+					this.#mouseState = true
+				}
+			})
+			window.addEventListener('mouseup', (e) => {
+				if (this.#isInsideDrum(p5.mouseX, p5.mouseY) && e.button === 0) {
+					this.mouseUp && this.mouseUp(this.#polarCoords(p5.mouseX, p5.mouseY))
+					this.#mouseState = false
+				}
+			})
+			window.addEventListener('mousemove', (e) => {
+				if (this.#isInsideDrum(p5.mouseX, p5.mouseY) && e.buttons === 1) {
+					this.mouseDrag && this.mouseDrag(this.#polarCoords(p5.mouseX, p5.mouseY))
+					this.#mouseState = true
+				}
+				if (!this.#isInsideDrum(p5.mouseX, p5.mouseY) && e.buttons === 1 && this.#mouseState) {
+					this.mouseDragExit && this.mouseDragExit(this.#polarCoords(p5.mouseX, p5.mouseY))
+					this.#mouseState = false
+				}
+			})
 		}
 
 		render = (dimensions) => {
@@ -59,8 +68,6 @@ export default function Drum(props, p5) {
 			this.x = dimensions.x
 			this.y = dimensions.y
 			this.diameter = dimensions.diameter
-			// handle events
-			this.handleEvents()
 			// return ellipse
 			p5.strokeWeight(6)
 			p5.ellipse(this.x, this.y, this.diameter)
