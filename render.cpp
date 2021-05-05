9796d0bca8ef0befcc2899ee3cfc91508e6e3486
lewiswolf <lewiswolstanholme@gmail.com>
2021-05-05 21:01:18 +0100

    better comments

--- render.cpp
+++ render.cpp
@@ -3,23 +3,20 @@
 
 Gui gui;
 
-float gSize = 0;			// R (size of drum)
-float gDecay = 1000;		// decay time in ms
-
 bool setup(BelaContext *context, void *userData) {
 	// init gui
 	gui.setup(context->projectName);
-	gui.setBuffer('f', 1);	// R
-	gui.setBuffer('f', 1);	// decay
-	gui.setBuffer('f', 3);	// mouse events (event type, r, theta)
+	gui.setBuffer('f', 1);	// R (size of drum)
+	gui.setBuffer('f', 1);	// decay time in ms
+	gui.setBuffer('f', 3);	// mouse or touch event (event type, r, theta)
 							// 0 = mousedown, 1 = mouseup, 2 = drag, 3 = dragexit
 	return true;
 }
 
 void render(BelaContext *context, void *userData) {
-	float R = gui.getDataBuffer(0).getAsFloat()[0];
-	float decay = gui.getDataBuffer(1).getAsFloat()[0];
-	float* event = gui.getDataBuffer(2).getAsFloat();
+	float R = gui.getDataBuffer(0).getAsFloat()[0];			// R (size of drum)
+	float decay = gui.getDataBuffer(1).getAsFloat()[0];		// decay time in ms
+	float* event = gui.getDataBuffer(2).getAsFloat();		// mouse or touch event (event type, r, theta)
                     
 	rt_printf("size %f \n", R);
 	rt_printf("decay %f \n", decay);
