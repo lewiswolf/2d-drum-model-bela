8873c239fe797f56acfd060e9ec1fd3b09ed5dbc
lewiswolf <lewiswolstanholme@gmail.com>
2021-05-12 19:45:23 +0100

    added default values and fixed a bug with maxmsp-gui

--- react-app/src/components/App.jsx
+++ react-app/src/components/App.jsx
@@ -16,17 +16,22 @@ export default class App extends React.Component {
 	constructor(props) {
 		super(props)
 		this.state = {
-			belaLoaded: Bela.readyState === 1 ? true : false, // is the Bela ws connected?
-			radiusOfDrum: 0,
-			decayTime: 0,
+			belaLoaded: true,
+			// belaLoaded: Bela.readyState === 1 ? true : false, // is the Bela ws connected?
+			radiusOfDrum: 100,
+			decayTime: 1000,
 		}
 	}
 
 	componentDidMount() {
 		// listeners for when Bela is connected/disconnected
-		window.addEventListener('BelaConnected', () => this.setState({ belaLoaded: true }))
+		window.addEventListener('BelaConnected', () => {
+			this.setState({ belaLoaded: true })
+			Bela.sendBuffer(0, 'float', this.state.radiusOfDrum)
+			Bela.sendBuffer(1, 'float', this.state.decayTime)
+		})
 		window.addEventListener('BelaDisconnected', () =>
-			this.setState({ belaLoaded: false, radiusOfDrum: 0, decayTime: 0 })
+			this.setState({ belaLoaded: false, radiusOfDrum: 100, decayTime: 1000 })
 		)
 	}
 
@@ -44,9 +49,10 @@ export default class App extends React.Component {
 								<p>{`Radius of Drum: ${this.state.radiusOfDrum}cm`}</p>
 								<Slider
 									ariaLabel='slider controlling the radius of a drum'
-									fidelity={10000}
-									value={this.state.radiusOfDrum}
+									fidelity={99}
+									value={this.state.radiusOfDrum - 1}
 									onChange={(i) => {
+										i++
 										Bela.sendBuffer(0, 'float', i)
 										this.setState({ radiusOfDrum: i })
 									}}
@@ -56,9 +62,10 @@ export default class App extends React.Component {
 								<p>{`Decay Time: ${this.state.decayTime}ms`}</p>
 								<Slider
 									ariaLabel='slider copntrolling the decay time of a sound'
-									fidelity={10000}
-									value={this.state.decayTime}
+									fidelity={9980}
+									value={2000}
 									onChange={(i) => {
+										i += 20
 										Bela.sendBuffer(1, 'float', i)
 										this.setState({ decayTime: i })
 									}}
